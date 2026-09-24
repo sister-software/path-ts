e2af28024547cf495d8c6aab43b73e1bd356a4b6
Teffen Ellis <592134+GirlBossRush@users.noreply.github.com>
2026-09-24 05:56:01 +0200

    Fix types.

--- index.ts
+++ index.ts
@@ -4,6 +4,15 @@
  * @author Teffen Ellis, et al.
  */
 
+// oxlint-disable typescript/triple-slash-reference
+
+/// <reference path="./lib/path-builder.ts" />
+
+import type { PathBuilder } from "./lib/path-builder.js"
+
+// This may look redundant, but it ensures that TypeScript picks up the type definitions for PathBuilder when this module is imported.
+export { PathBuilder, type PathBuilderLike, type UnwrapPathBuilder } from "./lib/path-builder.js"
+
 export * from "./lib/basename.js"
 export * from "./lib/dirname.js"
 export * from "./lib/extname.js"
@@ -17,3 +26,15 @@ export * from "./lib/relative.js"
 export * from "./lib/resolve.js"
 export * from "./lib/sep.js"
 export * from "./lib/type-utils.js"
+
+// oxlint-disable-next-line typescript/no-useless-empty-export -- Enforces ESM module semantics.
+export {}
+
+declare global {
+	interface StringConstructor {
+		/**
+		 * @deprecated use {@linkcode PathBuilder#toString}
+		 */
+		(value: PathBuilder): never
+	}
+}
